61d8dcfea50d2fb92133f598c24502ceda010e48
JaFaR <sysadjafarpr@gmail.com>
2020-03-01 21:51:51 +0500

    initial commit

---
+++ cmd/archivator/constants.go
@@ -0,0 +1,6 @@
+package main
+
+const fileInputPath ="cmd/archivator/files/"
+const fileOutputSeqPath ="cmd/archivator/zips/seqZip/"
+const fileOutputConPath ="cmd/archivator/zips/conZip/"
+const fileTypeZip  =".zip"
